7257f494c07b38ea8c969f6070b3d66d24a164b7
Roxanne-Mary Bell <96121350+roxanne007@users.noreply.github.com>
2025-08-06 15:36:53 +1000

    Update stamptool.js

--- tools/stamptool.js
+++ tools/stamptool.js
@@ -2,17 +2,19 @@ function StampTool() {
   this.name = "stampTool";
   this.icon = "assets/stamp.jpg";
   this.size = 50;
-  this.currentStamp = loadImage("assets/smileyface.jpg"); // default
+  this.currentStamp = loadImage("assets/smileyface.jpg"); // default stamp
+
+  this.setStamp = function (imgSrc) {
+    loadImage(imgSrc, (img) => {
+      this.currentStamp = img;
+    });
+  };
 
   this.draw = function () {
-    if (mouseIsPressed) {
+    if (mouseIsPressed && this.currentStamp) {
       let x = mouseX - this.size / 2;
       let y = mouseY - this.size / 2;
       image(this.currentStamp, x, y, this.size, this.size);
     }
-  };
-
-  this.setStamp = function (src) {
-    this.currentStamp = loadImage(src);
   };
 }
