8c0087c201b96516283aee7993945d29af042aec
Roxanne-Mary Bell <96121350+roxanne007@users.noreply.github.com>
2025-08-06 15:31:35 +1000

    Update stamptool.js

--- tools/stamptool.js
+++ tools/stamptool.js
@@ -1,31 +1,18 @@
-var stamp;
-//var stampSizeSlider;
-function StampTool()
+function StampTool() {
+  this.name = "stampTool";
+  this.icon = "assets/stamp.jpg";
+  this.size = 50;
+  this.currentStamp = loadImage("assets/smileyface.jpg"); // default
 
-{
-	
-	this.name = "stampTool";
-    this.icon = "assets/stamp.jpg";
-    this.size = 50;
-    let thisStamp = loadImage ('./assets/smileyface.jpg');
-//    let stampSlider = null ;
-    let self = this;
-//      
-//stampSizeSlider = createSlider (5, 50,20); 
-// stampSizeSlider.parent("#sizeofStampControl");
-
-
-this.draw = function()
-{
-    
-    if(mouseIsPressed)
-    { 
-        
-        var stampToolX= mouseX - this.size/2;
-        var stampToolY= mouseY - this.size/2;
-        image(thisStamp, stampToolX, stampToolY, 
-             this.size,this.size);
+  this.draw = function () {
+    if (mouseIsPressed) {
+      let x = mouseX - this.size / 2;
+      let y = mouseY - this.size / 2;
+      image(this.currentStamp, x, y, this.size, this.size);
     }
-        
-    };
+  };
+
+  this.setStamp = function (src) {
+    this.currentStamp = loadImage(src);
+  };
 }
